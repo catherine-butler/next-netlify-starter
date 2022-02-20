8c8d4e1ca5746c67827817245dbac3999fd8eaf9
Catherine Butler <catherinebutler@catherines-air.home>
2022-02-20 23:16:06 +0000

    debugging picture test to header

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
-import "Header.css";
+import styles from "./Header.css";
 
-export default function Header({ title, styles }) {
+export default function Header({ title }) {
   return (
     <>
       <h1 className="title">{title}</h1>
