8234870f02f8983ece5b2a4f5dfda40a6f66855a
Catherine Butler <catherinebutler@catherines-air.home>
2022-02-20 22:08:14 +0000

    updated title to match my project

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Essential Wedding Info</title>
+        /* <link rel="icon" href="/favicon.ico" /> */
       </Head>
 
       <main>
@@ -19,5 +19,5 @@ export default function Home() {
 
       <Footer />
     </div>
-  )
+  );
 }
