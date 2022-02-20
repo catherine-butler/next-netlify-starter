c0c7d115cf48e66d2acb9a7ea818eff95db27cbb
Catherine Butler <catherinebutler@catherines-air.home>
2022-02-20 23:38:59 +0000

    test added larger sentence

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,17 @@ export default function Home() {
         <h3>subtitle test</h3>
         <p className="description">this will be some additional text</p>
         <h4>subtitle test</h4>
-        <p className="description">this will be some more text</p>
+        <p className="description">
+          {" "}
+          Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book. It has survived not only five centuries,
+          but also the leap into electronic typesetting, remaining essentially
+          unchanged. It was popularised in the 1960s with the release of
+          Letraset sheets containing Lorem Ipsum passages, and more recently
+          with desktop publishing
+        </p>
       </main>
 
       <Footer />
