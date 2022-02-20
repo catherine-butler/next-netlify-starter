3eccc7d33480283a4d04f1a53bea06a9f0789ff3
Catherine Butler <catherinebutler@catherines-air.home>
2022-02-20 23:28:09 +0000

    removed picture test from header component and test added images to public

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,5 @@
 import styles from "./Header.css";
 
 export default function Header({ title }) {
-  return (
-    <>
-      <h1 className="title">{title}</h1>
-      <header className={styles.topimage}>
-        <img
-          src="./styles/images/couple-shot.jpeg"
-          alt="couple shot ben + emma"
-          className={styles.topimage}
-        />
-      </header>
-    </>
-  );
+  return <h1 className="title">{title}</h1>;
 }
