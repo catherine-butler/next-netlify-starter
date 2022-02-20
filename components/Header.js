4ddb5f2e5548fa5d025124ff241484b8c0f876df
Catherine Butler <catherinebutler@catherines-air.home>
2022-02-20 23:17:55 +0000

    debugged inconsistent naming from header.js for img

--- components/Header.js
+++ components/Header.js
@@ -4,11 +4,11 @@ export default function Header({ title }) {
   return (
     <>
       <h1 className="title">{title}</h1>
-      <header className={styles.header}>
+      <header className={styles.topimage}>
         <img
           src="./styles/images/couple-shot.jpeg"
           alt="couple shot ben + emma"
-          className={styles.top_image}
+          className={styles.topimage}
         />
       </header>
     </>
