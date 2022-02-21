0d637fb7d11c6431852b73ad75c06f06338743e7
Catherine Butler <catherinebutler@catherines-air.home>
2022-02-21 20:33:06 +0000

    renamed img src searching for bug

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         <img
-          src="../public/festival-flags.svg"
+          src="/festival-flags.svg"
           alt="Multicoloured festival flags"
           className={styles.flags}
         />
