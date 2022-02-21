9b04bcf6b98fe6355069a4cf300c9dbac2353f3b
Catherine Butler <catherinebutler@catherines-air.home>
2022-02-21 20:07:17 +0000

    added picture to footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,11 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        We really hope you can make it - please RVSP to{" "}
-        <span style="color: #0125f0">emmaperry20@hotmail.com</span> by April
-        30th 2022.
+        <img
+          src="../images/festival-flags.svg"
+          alt="Multicoloured festival flags"
+          className={styles.flags}
+        />
       </footer>
     </>
   );
