b9daa31f7e22560429d2f539634575c1cd790b34
Catherine Butler <catherinebutler@catherines-air.home>
2022-02-21 21:05:40 +0000

    added require to src in footer component and debugged flag class in footer css

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         <img
-          src="/festival-flags.svg"
+          src={require("/festival-flags.svg")}
           alt="Multicoloured festival flags"
           className={styles.flags}
         />
