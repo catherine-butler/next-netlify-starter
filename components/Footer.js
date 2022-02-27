02f59ed7dd805820c9bd1787c9b89e8d85e6ee26
Catherine Butler <catherinebutler@Catherines-Air.lan>
2022-02-27 17:10:11 +0000

    adjusted footer text sizing

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        We really hope you can make it - please RVSP to emmaperry20@hotmail.com
-        by April 30th 2022.
+        We really hope you can make it - please RVSP to{" "}
+        <b>emmaperry20@hotmail.com</b> by April 30th 2022.
       </footer>
     </>
   );
